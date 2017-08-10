07113b8f78bab3e72fe3d5d7ff755df824c9b286
ThomasKV <30895483+ThomasKV@users.noreply.github.com>
2017-08-10 14:04:38 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // This remark was inserted using GitHub
+            // Another remark inserted using GitHub
         }
     }
 }
